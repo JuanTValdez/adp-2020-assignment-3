2eb2423607a0703a9f28c57b1e6f67127f112dfe
JuanValdez <juantvaldez85@gmail.com>
2020-03-11 15:05:31 -0400

    feat: added index.js. Contains ReactDom.render.

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import 'regenerator-runtime/runtime'
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App.js'
+
+ReactDom.render(
+    <App/>,
+    document.getElementById('root')
+
+);
