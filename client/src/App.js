d1529754ec41468de65528d8dd3d9d0c011ed2dc
JuanValdez <juantvaldez85@gmail.com>
2020-03-12 13:50:14 -0400

    feat: signup pages and signup page css

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm} from 'react-hook-form'
 import { GraphQL, GraphQLProvider} from 'graphql-react'
 import 
-    {   Browser as Router,
-        Router,
+    {   BrowserRouter as Router,
+        Switch,
         Route,
-        Link
-    } from "react-router-d"
+        Link,
+        useHistory
+    } from 'react-router-dom'
+import {
+    TextField,
+    Button,
+    Typography
+} from '@material-ui/core';
+import './App.css';
 
 const graphql = new GraphQL();
 
 const App = () => {
+
+    // This is for forms
+    const { register, errors } = useForm();
+    const [serverError, setServerError] = useState();
+
     return(
        
        <GraphQLProvider graphql={graphql}>
 
+           
+           <div className="Container">
 
+               <h1>Sign-Up Form</h1> 
+                <form>
 
+                <TextField
+                    name="Create Username"
+                    label="Username"
+                    fullWidth
+                    inputRef={register({
+                        required: 'Required'})}
+                    error={errors.username != null}
+                >
+                </TextField>
+                    <Typography color="error">
+                        {errors.username && errors.username.message}
+                    </Typography>
+
+                <TextField
+                    name="password"
+                    label="Password"
+                    type="password"
+                    fullWidth
+                    inputRef={register({
+                        required: 'Required',
+                    })}
+                    error={errors.pasword != null}
+                >
+                </TextField>
+                <Typography color="error">
+                    {errors.password && errors.password.message}
+                </Typography>
+
+                <Button 
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  fullWidth
+                >
+                    Log In
+                </Button>
+
+                </form>
+
+           </div>
 
 
 
