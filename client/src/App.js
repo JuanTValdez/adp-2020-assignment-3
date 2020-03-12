92149eadb1852d408e386a0194090a84ba9772a5
JuanValdez <juantvaldez85@gmail.com>
2020-03-12 18:59:10 -0400

    feat: converted Signup page into component. Added width styling

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import {
     Button,
     Typography
 } from '@material-ui/core';
+import Signup from './Signup.js'
 import './App.css';
 
 const graphql = new GraphQL();
@@ -27,52 +28,9 @@ const App = () => {
        
        <GraphQLProvider graphql={graphql}>
 
-           
-           <div className="Container">
+           <Signup />
 
-               <h1>Sign-Up Form</h1> 
-                <form>
-
-                <TextField
-                    name="Create Username"
-                    label="Username"
-                    fullWidth
-                    inputRef={register({
-                        required: 'Required'})}
-                    error={errors.username != null}
-                >
-                </TextField>
-                    <Typography color="error">
-                        {errors.username && errors.username.message}
-                    </Typography>
-
-                <TextField
-                    name="password"
-                    label="Password"
-                    type="password"
-                    fullWidth
-                    inputRef={register({
-                        required: 'Required',
-                    })}
-                    error={errors.pasword != null}
-                >
-                </TextField>
-                <Typography color="error">
-                    {errors.password && errors.password.message}
-                </Typography>
-
-                <Button 
-                  variant="contained"
-                  color="primary"
-                  type="submit"
-                  fullWidth
-                >
-                    Log In
-                </Button>
-
-                </form>
-
-           </div>
+    
 
 
 
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,19 +1,71 @@
 import React, { useState } from 'react'
-import { useForm} from 'react-hook-form'
-import { GraphQL, GraphQLProvider } from 'graphql-react'
+import { useForm, ErrorMessage} from 'react-hook-form'
 
 import {
     TextField,
     Button,
     Typography,
-} from '@material-ui/core';
+} from '@material-ui/core'
+import './App.css'
 
-const graphql = new GraphQL()
 
 const Signup = () => {
 
+    const { handleSubmit, register, errors } = useForm();
+    const [serverError, setServerError] = useState();
+    const onSubmit = async values => {
 
+        setServerError(null)
 
+    }
+
+    return (
+        <div className="Container">
+            <h1> Sign-Up Form</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <TextField
+                    name="username"
+                    label="Username"
+                    fullWidth
+                    inputRef={register({
+                        required: 'Required'})}
+                        error={errors.username != null}
+                >
+               </TextField>
+                    <Typography color ='error'>
+                        {errors.username && errors.username.message}
+                    </Typography>     
+                <TextField
+                    name="password"
+                    label="Password"
+                    type="password"
+                    fullWidth
+                    inputRef={register({
+                        required: 'Required',
+                    })}    
+                    error={errors.password != null}
+                >
+
+                </TextField>
+                <Typography color="error">
+                    {errors.password && errors.password.message}
+                </Typography>
+                <Typography color="error">
+                    {serverError}
+                </Typography>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    fullWidth
+                >
+                    Log In
+                </Button>
+            </form>
+
+            
+        </div>
+    )
 
 
 }
