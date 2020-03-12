155256fb08569435ee97046c73f5238630040ffa
JuanValdez <juantvaldez85@gmail.com>
2020-03-12 16:25:00 -0400

    feat: start signup.js component

---
+++ client/src/Signup.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { useForm} from 'react-hook-form'
+import { GraphQL, GraphQLProvider } from 'graphql-react'
+
+import {
+    TextField,
+    Button,
+    Typography,
+} from '@material-ui/core';
+
+const graphql = new GraphQL()
+
+const Signup = () => {
+
+
+
+
+
+}
+
+export default Signup
