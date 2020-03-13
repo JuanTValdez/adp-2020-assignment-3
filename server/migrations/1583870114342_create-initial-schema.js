c121e3c18f3e4f8bf6e2a110099b83650ce2aaec
JuanValdez <juantvaldez85@gmail.com>
2020-03-13 16:23:06 -0400

    feat: started creating tables via script

--- server/migrations/1583870114342_create-initial-schema.js
+++ server/migrations/1583870114342_create-initial-schema.js
@@ -2,6 +2,27 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = pgm => {
+  pgm.createTable("country_of_origin",{
+    id: 'id',
+    name: { type: 'text', notNull: true },
+    area_cardinal_direction: { type: 'text', notNull: true},
+    terran: { type: 'text', notNull: true}
+
+
+  });
+
+  pgm.createTable("food_list", {
+    id: "id",
+    name: { type: "text", notNull: true },
+    price: { type: "float", notNull: true },
+    vegetarian_vegan: { type: "text", notNull: true },
+    spice_level: { type: "int", notNull: true },
+    gluten_free: { type: "text", notNull: true },
+    soy_free: { type: "text", notNull: true },
+    country_origin_id: { type: "int", notNull: true, references: "" }
+  });
+
+};
 
 exports.down = pgm => {};
